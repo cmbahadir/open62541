3a75c7fd9e121d1319a1d3e826c6ffe883f79e3e
cmbahadir <c.mete.bahadir@gmail.com>
2020-10-03 09:45:31 +0300

    sendError tests.

--- tests/check_securechannel.c
+++ tests/check_securechannel.c
@@ -164,6 +164,14 @@ START_TEST(SecureChannel_sendAsymmetricOPNMessage_SecurityModeInvalid) {
 }
 END_TEST
 
+START_TEST(SecureChannel_sendError) {
+    UA_TcpErrorMessage errMsg;
+    UA_TcpErrorMessage_init(&errMsg);
+    errMsg.error = UA_STATUSCODE_BADSECURITYMODEREJECTED;
+    UA_Connection_sendError(testChannel.connection, &errMsg);
+}
+END_TEST
+
 START_TEST(SecureChannel_sendAsymmetricOPNMessage_SecurityModeNone) {
     // Configure our channel correctly for OPN messages and setup dummy message
     UA_OpenSecureChannelResponse dummyResponse;
@@ -572,6 +580,10 @@ testSuite_SecureChannel(void) {
     tcase_add_test(tc_sendSymmetricMessage, SecureChannel_sendSymmetricMessage);
     tcase_add_test(tc_sendSymmetricMessage, SecureChannel_sendSymmetricMessage_invalidParameters);
     tcase_add_test(tc_sendSymmetricMessage, SecureChannel_sendSymmetricMessage_modeNone);
+
+    TCase *tc_sendError = tcase_create("Test sendError function");
+    tcase_add_test(tc_sendError, SecureChannel_sendError);
+
 #ifdef UA_ENABLE_ENCRYPTION
     tcase_add_test(tc_sendSymmetricMessage, SecureChannel_sendSymmetricMessage_modeSign);
     tcase_add_test(tc_sendSymmetricMessage, SecureChannel_sendSymmetricMessage_modeSignAndEncrypt);
